1f1b0df16369430af8b615324ec7bfb6cd9d65e1
Mickey <mickeyc11111@gmail.com>
2022-10-26 19:48:01 -0400

    commit tree e6bf6d3c9b97822720a44367996d6b309c1be535

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    "#{self.actors[0].first_name} #{self.actors[0].last_name}"
+  end
 end
